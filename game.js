5f69ff1038683dffbed0d96802258c963a989a49
Lucas-Dyck <lucasedyck@gmail.com>
2026-06-18 14:11:53 -0400

    Update game.js

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ function checkAnswer(currentLevel){
             setTimeout(() => {nextSequence(), 1000})
         }
     } else {
-        new Audio("./wrong.mp3").play();
+        new Audio("./sounds/wrong.mp3").play();
         $("body").css("background-image", "linear-gradient(red,red)");
         setTimeout(() => {$("body").css("background-image", "url(./tabletop.jpg)");}, 200)
         console.log("wrong.")
@@ -48,8 +48,8 @@ function checkAnswer(currentLevel){
 }
 
 function startOver(){
-    $("#level-title").text("Game Over, Press the Centre to Restart");
     $("p").text(saveHighestLevel(level));
+    $("#level-title").text("Game Over, Press the Centre to Restart");
     gamePattern = [];
     started = false;
     level = 0;
@@ -83,7 +83,7 @@ function nextSequence(){
     
 }
 function playSound(colour){
-    new Audio("./" + colour + ".mp3").play();
+    new Audio("./sounds/" + colour + ".mp3").play();
 }
 
 function animatePress(Colour){
